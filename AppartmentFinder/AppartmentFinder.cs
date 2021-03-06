8fc41fe188c5159a88dd88a1b512af46cbe916dd
Sergey Benzenko <sbenzenko@gmail.com>
2021-03-06 23:04:29 +0300

    Appartment Finder task

---
+++ AppartmentFinder/AppartmentFinder.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace AppartmentFinder
+{
+  public class AppartmentFinder
+  {
+    public int Find(Dictionary<string, bool>[] blocks)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
