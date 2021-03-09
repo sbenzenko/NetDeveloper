530f9eb3dfa96a6b32e855c860e7a68e11e3f7f5
Sergey Benzenko <sbenzenko@gmail.com>
2021-03-09 21:20:21 +0300

    Apartment Finder solution

---
+++ ApartmentFinderSolution/ApartmentFinder.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ApartmentFinder
+{
+  public class ApartmentFinder
+  {
+    const int BIGGEST_DISTANCE = 999999;
+    private int[,] distances;
+
+    private enum Position
+    {
+      Previous = -1,
+      Current = 0,
+      Next = 1
+    }
+
+    public int Find(Dictionary<string, bool>[] blocks)
+    {
+      distances = new int[blocks.Length, blocks[0].Count];
+      int blocksCount = distances.GetLength(0);
+      var maxDistances = new int[blocksCount];
+
+      PopulateBlockDistances(blocks[0],
+  0, Position.Current, true);
+
+      for (int block = 1; block < blocksCount; block++)
+      {
+        PopulateBlockDistances(blocks[block],
+          block, Position.Previous, true);
+      }
+
+      maxDistances[blocksCount - 1] = MaxServiceDistance(blocksCount - 1);
+
+      for (int block = blocksCount - 2; block >= 0; block--)
+      {
+        PopulateBlockDistances(blocks[block],
+          block, Position.Next);
+        maxDistances[block] = MaxServiceDistance(block);
+      }
+
+      return FindMinBlock(maxDistances);
+    }
+
+    private int PopulateBlockDistances(Dictionary<string, bool> services,
+      int currentBlock, Position compareToBlock, bool initialize = false)
+    {
+      int i = 0;
+      foreach (var service in services.Values)
+      {
+        if (initialize)
+          distances[currentBlock, i] = BIGGEST_DISTANCE;
+
+        distances[currentBlock, i] = service ? 0 :
+            Math.Min(distances[currentBlock, i], distances[currentBlock + (int)compareToBlock, i] + 1);
+        i++;
+      }
+
+      return i;
+    }
+
+    private int FindMinBlock(int[] serviceDistances)
+    {
+      int min = BIGGEST_DISTANCE;
+      int index = -1;
+      for (int i = 0; i < serviceDistances.Length; i++)
+      {
+        if (serviceDistances[i] < min)
+        {
+          min = serviceDistances[i];
+          index = i;
+        }
+      }
+      return index;
+    }
+
+    private int MaxServiceDistance(int index)
+    {
+      int max = 0;
+      for (int i = 0; i < distances.GetLength(1); i++)
+      {
+        if (distances[index, i] > max) max = distances[index, i];
+      }
+
+      return max;
+    }
+  }
+}
---
+++ ApartmentFinderSolution/Tests.cs
@@ -0,0 +1,186 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace ApartmentFinder
+{
+  public class Tests
+  {
+    [Test]
+    public void SimplestCase()
+    {
+      var blocks = new Dictionary<string, bool>[2]
+      {
+        new Dictionary<string, bool>()
+          {
+            { "gym", false }
+          },
+        new Dictionary<string, bool>()
+          {
+            { "gym", true }
+          }
+      };
+
+      var finder = new ApartmentFinder();
+      int result = finder.Find(blocks);
+
+      Assert.AreEqual(1, result);
+    }
+
+    [Test]
+    public void SimpleCase()
+    {
+      var blocks = new Dictionary<string, bool>[2]
+      {
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }
+        }
+      };
+
+      var finder = new ApartmentFinder();
+      int result = finder.Find(blocks);
+
+      Assert.AreEqual(0, result);
+    }
+
+    [Test]
+    public void OriginalCase()
+    {
+      var blocks = new Dictionary<string, bool>[5]
+      {
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", true }, { "store", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", true }, { "store", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", true }
+        }
+      };
+
+      var finder = new ApartmentFinder();
+      int result = finder.Find(blocks);
+
+      Assert.AreEqual(3, result);
+    }
+
+    [Test]
+    public void HarderCase()
+    {
+      var blocks = new Dictionary<string, bool>[8]
+      {
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", true }, { "store", false }, { "carwash", true }, { "laundry", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", true }, { "carwash", false }, { "laundry", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", true }, { "carwash", true }, { "laundry", false }
+        }
+      };
+
+      var finder = new ApartmentFinder();
+      int result = finder.Find(blocks);
+
+      Assert.AreEqual(3, result);
+    }
+
+    [Test]
+    public void InsaneCase()
+    {
+      var blocks = new Dictionary<string, bool>[20]
+      {
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", true }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", true }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", true }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", true }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", true }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", true }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", true }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", true }, { "movie", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", true }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", true }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", true }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", true }, { "kindergarten", false }, { "hospital", false }, { "movie", true }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", true }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", true }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", false }, { "carwash", true }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", false }, { "store", true }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", false }, { "school", true }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        },
+        new Dictionary<string, bool>() {
+          { "gym", true }, { "school", false }, { "store", false }, { "carwash", false }, { "laundry", false }, { "drugstore", false }, { "theater", false }, { "kindergarten", false }, { "hospital", false }, { "movie", false }
+        }
+      };
+
+      var finder = new ApartmentFinder();
+      int result = finder.Find(blocks);
+
+      Assert.AreEqual(15, result);
+    }
+  }
+}
